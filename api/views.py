a8af3b7b9fe434579188b99509925e77d279b417
Ranjeet3345 <yadavranjeet7770@gmail.com>
2025-05-28 11:24:17 +0530

    Serializer defined,also super user has been made

--- api/views.py
+++ api/views.py
@@ -1,2 +1,8 @@
 from django.shortcuts import render
+from .serializers import CustomerSerializer
+from .models import Customer
+from rest_framework.generics import ListAPIView
 
+class CustomerList(ListAPIView):
+    queryset=Customer.objects.all()
+    serializer_class=CustomerSerializer
