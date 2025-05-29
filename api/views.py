083b082f5b316ef2450eba58e4b4570462d510ba
Ranjeet3345 <yadavranjeet7770@gmail.com>
2025-05-29 15:01:00 +0530

    Pagination with search and sort ing facility

--- api/views.py
+++ api/views.py
@@ -1,45 +1,31 @@
-# from django.shortcuts import render
-# from .serializers import CustomerSerializer
-# from .models import Customer
-# from rest_framework.generics import ListAPIView
-# from .mypagination import myPageNumberPagination
-# from rest_framework import filters
-
-# class CustomerList(ListAPIView):
-#     queryset=Customer.objects.all()
-#     serializer_class=CustomerSerializer
-#     pagination_class=myPageNumberPagination
 
 from django.shortcuts import render
 from .serializers import CustomerSerializer
 from .models import Customer
 from rest_framework.generics import ListAPIView
 from .mypagination import myPageNumberPagination
+from django.db.models import Q
 
 class CustomerList(ListAPIView):
     serializer_class = CustomerSerializer
-    print(serializer_class)
-    print("data=----")
-    serializer = CustomerSerializer(Customer.objects.first())
-    print(serializer.data)
     pagination_class = myPageNumberPagination
-    print(myPageNumberPagination)
+
     def get_queryset(self):
         queryset = Customer.objects.all()
-        print("queryset---started------")
-        print(queryset)
-        print("queryset--ended--")
-        sort_by = self.request.query_params.get('sort_by')
-        print("sorted by --")
-        print(sort_by)
 
-        # Allow sorting only on safe fields
+        # Search
+        search_query = self.request.query_params.get('search')
+        if search_query:
+            queryset = queryset.filter(
+                Q(cname__icontains=search_query) | Q(cemail__icontains=search_query)
+            )
+
+        # Sort
+        sort_by = self.request.query_params.get('sort_by')
         allowed_sort_fields = ['cname', 'cemail', '-cname', '-cemail']
         if sort_by in allowed_sort_fields:
             queryset = queryset.order_by(sort_by)
-        
-        print("sorted---queryset------")
-        print(queryset)
+
         return queryset
 
     
